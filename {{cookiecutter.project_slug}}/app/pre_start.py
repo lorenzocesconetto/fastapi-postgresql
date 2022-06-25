c92467f62c1336f5084f82e7aae22f46466b01f0
Lorenzo C <lorenzo_cesconeto@hotmail.com>
2022-06-25 20:10:26 -0300

    Configurations

--- {{cookiecutter.project_slug}}/app/pre_start.py
+++ {{cookiecutter.project_slug}}/app/pre_start.py
@@ -1,14 +1,15 @@
 import logging
 
+from tenacity import after_log, before_log, retry, stop_after_attempt, wait_fixed
+
 from app.db.init_db import init_db
 from app.db.session import SessionLocal
-from tenacity import after_log, before_log, retry, stop_after_attempt, wait_fixed
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-wait_seconds = 1  # one second between retries
-max_tries = 60 * 2  # 120 retries -> two minutes
+wait_seconds = 2  # two seconds between retries
+max_tries = 30  # 30 retries -> one minute
 
 
 @retry(
@@ -18,6 +19,7 @@ max_tries = 60 * 2  # 120 retries -> two minutes
     after=after_log(logger, logging.WARN),
 )
 def init() -> None:
+    logger.info("Initializing service")
     try:
         db = SessionLocal()
         # Try to create session to check if DB is awake
@@ -26,9 +28,4 @@ def init() -> None:
         logger.error(e)
         raise e
     init_db(db)
-
-
-def main() -> None:
-    logger.info("Initializing service")
-    init()
     logger.info("Service finished initializing")
